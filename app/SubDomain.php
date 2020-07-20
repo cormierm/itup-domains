9db954c243d3d3fb3d770d1290746a906a203ef0
Matt Cormier <cormierm@gmail.com>
2020-07-20 16:24:45 -0400

    Adds sub domain model

---
+++ app/SubDomain.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SubDomain extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+}
