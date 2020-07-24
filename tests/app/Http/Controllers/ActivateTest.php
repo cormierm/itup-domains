5bde827d7746ee3f4e43923ce492a656682052b0
Matt Cormier <cormierm@gmail.com>
2020-07-24 14:04:29 -0400

    Adds test coverage for activate

---
+++ tests/app/Http/Controllers/ActivateTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\App\Http\Controllers;
+
+use App\Hostname;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ActivateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function itWillReturnErrorIfTokenDoesNotExist(): void
+    {
+
+        $this->getJson(route('activate', 'invalid-token-uuid'))
+            ->assertViewIs('home')
+            ->assertViewHas('alert', [
+                'type' => 'error',
+                'text' => 'There was a problem activating. This hostname may have been already activated.'
+            ]);
+    }
+
+    /** @test */
+    public function itWillNotActivateExpired(): void
+    {
+
+        $hostname = factory(Hostname::class)->create([
+            'created_at' => Carbon::now()->subDays(2),
+            'verified_at' => null,
+        ]);
+
+        $this->getJson(route('activate', $hostname->token))
+            ->assertViewIs('home')
+            ->assertViewHas('alert', [
+                'type' => 'error',
+                'text' => 'There was a problem activating. This activation has expired. Please try registering again'
+            ]);
+    }
+
+    /** @test */
+    public function itWillActivateNonVerifiedHostnameWithValidToken(): void
+    {
+
+        $hostname = factory(Hostname::class)->create([
+            'verified_at' => null,
+        ]);
+
+        $this->getJson(route('activate', $hostname->token))
+            ->assertViewIs('home')
+            ->assertViewHas('alert', [
+                'type' => 'success',
+                'text' => 'Successfully activated hostname!'
+            ]);
+
+        $this->assertNotNull($hostname->fresh()->verified_at);
+    }
+}
