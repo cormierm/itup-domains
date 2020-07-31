19c13e6ef898477e68c562441702cc7289621fdd
Matt Cormier <cormierm@gmail.com>
2020-07-31 13:34:40 -0400

    Adds DeleteUnactivatedHostnames to laravel scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\DeleteUnactivatedHostnames;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,15 +17,9 @@ class Kernel extends ConsoleKernel
         //
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new DeleteUnactivatedHostnames)->everyMinute();
     }
 
     /**
